c26c944f75207077ba2ee93204942eefbbb482d1
W. Trevor King <wking@tremily.us>
2013-04-14 14:18:38 -0400

    Run update-copyright.py

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-# Copyright
+# Copyright (C) 2013 W. Trevor King <wking@tremily.us>
+#
+# This file is part of ipython-irc.
+#
+# ipython-irc is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# ipython-irc is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# ipython-irc.  If not, see <http://www.gnu.org/licenses/>.
 
 "An IRC iframe using webchat.freenode.net"
 
